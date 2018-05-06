bed432a3931911bcc7b4e76789192dd68377116c
flourishcodes <kunle@adeniran.com>
2018-05-05 20:11:18 -0700

    Unittest cases for max_integer

---
+++ 6-max_integer_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Module to find the max integer
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Test the max_integer function
+    """
+
+    def test_max_integer(self):
+        '''
+        Test if integer is max integer
+        '''
+        result = max_integer([1, 4, 3, 2])
+        self.assertEqual(result, 4)
+        result = max_integer([12, 9, 7, 2])
+        self.assertEqual(result, 12)
+
+    def test_isint(self):
+        '''
+        Test to check variable against integer
+        '''
+        var = 1
+        self.assertTrue(max_integer([var, 2]) == 2)
+        x = 5
+        self.assertTrue(max_integer([1, x]) == x)
+
+    def test_float_integer(self):
+        '''
+        Test to see if float is max integer
+        '''
+        self.assertEqual(max_integer([4.0, 3, 2]), 4.0)
+
+    def test_negative_integer(self):
+        '''
+        Test only negative integers
+        '''
+        self.assertEqual(max_integer([-1, -2, -5]), -1)
+
+    if __name__ == '__main__':
+        unittest.main()
